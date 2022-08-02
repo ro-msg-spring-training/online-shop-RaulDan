c1f31acc18e3248fd713282af0dd5852f13f5e6e
Raul Calugar <Raul.Calugar@msg.group>
2022-08-02 21:52:32 +0300

    Making Code Review And Finishing Chapter 4

--- shop/src/main/java/ro/msg/learning/shop/service/impl/strategy/SingleLocationStrategy.java
+++ shop/src/main/java/ro/msg/learning/shop/service/impl/strategy/SingleLocationStrategy.java
@@ -14,38 +14,39 @@ import ro.msg.learning.shop.service.StockService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class SingleLocationStrategy implements IStrategy {
-
     private final StockService stockService;
     private final LocationService locationService;
-    @Override
-    public List<Stock> createOrder(List<OrderDetail> products){
 
-        List<Location> locations=locationService.findAll();
-        boolean found=false;
-        for(Location location:locations){
-            int count=0;
-            List<Stock> stocks=stockService.findAllByLocation(location);
-            for(Stock currentStock:stocks){
-                found=false;
-                for(OrderDetail product:products){
-                    if(currentStock.getProduct().equals(product.getProduct()) && currentStock.getQuantity()>=product.getQuantity()){
+    @Override
+    public List<Stock> createOrder(List<OrderDetail> products) {
+
+        List<Location> locations = locationService.findAll();
+        boolean found = false;
+        for (Location location : locations) {
+            int count = 0;
+            List<Stock> stocks = stockService.findAllByLocation(location);
+            for (Stock currentStock : stocks) {
+                found = false;
+                for (OrderDetail product : products) {
+                    if (currentStock.getProduct().equals(product.getProduct()) && currentStock.getQuantity() >= product.getQuantity()) {
                         count++;
-                        found=true;
+                        found = true;
                         break;
                     }
                 }
-                if(found){
+                if (found) {
                     continue;
                 }
             }
-            if(count==products.size()){
-                for(Stock stock:stocks){
-                    for(OrderDetail product:products){
-                        if(stock.getProduct().equals(product.getProduct())){
-                            stockService.editQuantity(stock,product.getQuantity());
+            if (count == products.size()) {
+                for (Stock stock : stocks) {
+                    for (OrderDetail product : products) {
+                        if (stock.getProduct().equals(product.getProduct())) {
+                            stockService.editQuantity(stock, product.getQuantity());
                             break;
                         }
                     }
@@ -54,6 +55,5 @@ public class SingleLocationStrategy implements IStrategy {
             }
         }
         throw new MissingStockException("There is no sufficient stock to create the order you asked!");
-//        return new ArrayList<>();
     }
 }
